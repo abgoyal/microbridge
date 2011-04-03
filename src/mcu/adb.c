030871af9ab225e68e504bed75b1c7dc0d9bf572
inopia.spam <inopia.spam@2f6c07a0-d694-e0f2-3ff0-8239edbdd181>
2011-04-03 09:36:01 +0000

    Fixed the include directive for string.h

--- src/mcu/adb.c
+++ src/mcu/adb.c
@@ -11,8 +11,9 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License.#include <string.h>
+limitations under the License.
 */
+#include <string.h>
 #include "adb.h"
 
 // #define DEBUG
